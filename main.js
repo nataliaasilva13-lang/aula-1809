26d18014b88edbe2fe40cdbfc1bd21d79b05ad96
nataliaasilva13-lang <natalia.a.silva13@aluno.senai.br>
2026-09-18 16:35:16 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-console.log("oi");
+const livro = {
+  titulo: 'minha vida',
+  autor: 'Natália',
+  descrever() {
+    return `O livro "${this.titulo}" foi escrito por ${this.autor}.`;
+  }
+};
+
+console.log(livro.descrever());
